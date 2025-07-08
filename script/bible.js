92430c03ac289abe4963327a4c83c886c0508785
NathzDevs <smithashleydump123@gmail.com>
2025-07-08 08:18:09 +0300

    Create bible.js

---
+++ script/bible.js
@@ -0,0 +1,11 @@
+module.exports.config = {
+  name: "bible",
+  version: "1.0.0",
+  role: 0,
+  hasPrefix: true,
+  aliases: [],
+  description: "Fetch a Bible verse!",
+  usage: "bible",
+  credits: "Dale Mekumi/Ry",
+  cooldown: 3,
+};
