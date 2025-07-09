03325fc695b192225ab7c156e193e0af88d03ff6
NathzDevs <smithashleydump123@gmail.com>
2025-07-09 15:41:58 +0800

    Update unsend.js

--- script/unsend.js
+++ script/unsend.js
@@ -24,4 +24,4 @@ module.exports.run = function({ api, event, getText }) {
 	if (event.messageReply.senderID != api.getCurrentUserID()) return api.sendMessage(getText("returnCant"), event.threadID, event.messageID);
 	if (event.type != "message_reply") return api.sendMessage(getText("missingReply"), event.threadID, event.messageID);
 	return api.unsendMessage(event.messageReply.messageID);
-}
+};
